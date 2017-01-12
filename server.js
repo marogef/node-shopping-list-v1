9e7a207a2ea79ec50f8488668b5cd082fe2b6456
Faras Maroge <Goldencrane33@yahoo.com>
2017-01-12 02:33:07 -0500

    added recipes to server.js

--- server.js
+++ server.js
@@ -6,6 +6,11 @@ const bodyParser = require('body-parser');
 
 const {ShoppingList} = require('./models');
 
+const{Recipes} = require('./models');
+
+
+
+
 const jsonParser = bodyParser.json();
 const app = express();
 
@@ -18,10 +23,20 @@ ShoppingList.create('beans', 2);
 ShoppingList.create('tomatoes', 3);
 ShoppingList.create('peppers', 4);
 
+
+//add items to the model
+Recipes.create('chocolate milk', ['cocoa', 'milk', 'sugar']);
+
+
 // when the root of this router is called with GET, return
 // all current ShoppingList items
 app.get('/shopping-list', (req, res) => {
   res.json(ShoppingList.get());
+});
+
+
+app.get('/recipes', (req, res) => {
+   res.json(Recipes.get());
 });
 
 app.listen(process.env.PORT || 8080, () => {
